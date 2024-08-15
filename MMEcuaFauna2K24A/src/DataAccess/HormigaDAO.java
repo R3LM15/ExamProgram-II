905209e24d85cc701c5ab8a7c68c33f1d91567af
R3LM <118556218+R3LM15@users.noreply.github.com>
2024-08-15 12:43:10 -0500

    User Interface DAo

--- MMEcuaFauna2K24A/src/DataAccess/HormigaDAO.java
+++ MMEcuaFauna2K24A/src/DataAccess/HormigaDAO.java
@@ -1,5 +1,7 @@
 package DataAccess;
 
+import DataAccess.DTO.HormigaDTO;
+import Framework.MMException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,86 +12,92 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-import DataAccess.DTO.HormigaDTO;
 
-
-public class HormigaDAO extends SQLiteDataHelper implements IDAO<HormigaDTO> {
+public class HormigaDAO extends SQLiteDataHelper implements IDAO<HormigaDTO>{
 
     @Override
-    public boolean create(HormigaDAO entity) throws Exception {
-        //  2            ,'Masculino'   ,'tipos de sexualida'
-        String query = " INSERT INTO Catalogo (IdHormiga ,IdSexo ,IdProvincia, IdGenoAlimento, IdIngestaNativa, TipoHormiga, Estado) VALUES (?,?,?,?,?,?,?)";
+    public boolean create(HormigaDTO entity) throws Exception {
+        String query = " INSERT INTO Hormiga (IdSexo ,IdProvincia ,IdGenoAlimento,IdIngestaNativa,TipoHormiga,Estado ) VALUES (?,?,?,?,?,?)";
         try {
             Connection        conn  = openConnection();
             PreparedStatement pstmt = conn.prepareStatement(query);
-            pstmt.setInt(1, 1);   //
-            pstmt.setString(2, entity.getNombre());
-            pstmt.setString(3, entity.getDescripcion());
+            pstmt.setInt(1, entity.getIdSexo());
+            pstmt.setInt(2, entity.getIdProvincia());
+            pstmt.setInt(3, entity.getIdGenoAlimento());
+            pstmt.setInt(4, entity.getIdIngestaNativa());
+            pstmt.setString(5, entity.getEstado());
+            pstmt.setString(6, entity.getFechaCrea());
             pstmt.executeUpdate();
             return true;
         } 
         catch (SQLException e) {
-            throw e;  //new PatException(e.getMessage(), getClass().getName(), "create()");
+            throw new MMException(e.getMessage(), getClass().getName(), "create()");
         }
     }
 
     @Override
-    public List<PersonaTipoDTO> readAll() throws Exception {
-        List <PersonaTipoDTO> lst = new ArrayList<>();
-        String query =" SELECT IdCatalogo   "
-                     +" ,IdCatalogoTipo     "
-                     +" ,Nombre             "
-                     +" ,Descripcion        "
+    public List<HormigaDTO> readAll() throws Exception {
+        List <HormigaDTO> lst = new ArrayList<>();
+        String query =" SELECT ROW_NUMBER () OVER ( ORDER BY IdHormiga ) RowNum "
+                     +" ,IdHormiga          "
+                     +" ,IdSexo             "
+                     +" ,IdProvincia        "
+                     +" ,IdGenoAlimento     "
+                     +" ,IdIngestaNativa    "
+                     +" ,TipoHormiga        "
                      +" ,Estado             "
-                     +" ,FechaCreacion      "
-                     +" ,FechaModifica      "
-                     +" FROM    Catalogo    "
-                     +" WHERE   Estado ='A' "
-                     +" AND   IdCatalogoTipo = 1 ";  
+                     +" ,FechaCrea          "
+                     +" FROM    Hormiga     "
+                     +" WHERE   Estado ='A' ";  
         try {
             Connection conn = openConnection();         // conectar a DB     
             Statement  stmt = conn.createStatement();   // CRUD : select * ...    
             ResultSet  rs   = stmt.executeQuery(query);    // ejecutar la
             while (rs.next()) {
-                PersonaTipoDTO s = new PersonaTipoDTO( rs.getInt(1)     // IdCatalogo
-                                        ,rs.getInt(2)     // IdCatalogoTipo             
-                                        ,rs.getString(3)  // Nombre         
-                                        ,rs.getString(4)  // Descripcion      
-                                        ,rs.getString(5)  // Estado
-                                        ,rs.getString(6)  // FechaCreacion
-                                        ,rs.getString(7));// FechaModifica
-                lst.add(s);
+                HormigaDTO u = new HormigaDTO(rs.getInt(1), 
+                                              rs.getInt(2),
+                                              rs.getInt(3), 
+                                              rs.getInt(4),
+                                              rs.getInt(5),
+                                              rs.getString(6), 
+                                              rs.getString(7), 
+                                              rs.getString(8));// FechaModifica
+                lst.add(u);
             }
         } 
         catch (SQLException e) {
-            throw e; //new PatException(e.getMessage(), getClass().getName(), "readAll()");
+            throw new MMException(e.getMessage(), getClass().getName(), "readAll()");
         }
         return lst; 
     }
 
     @Override
-    public boolean update(PersonaTipoDTO entity) throws Exception {
+    public boolean update(HormigaDTO entity) throws Exception {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");  
         LocalDateTime now = LocalDateTime.now();
-        String query = "UPDATE Catalogo SET Nombre = ?, Descripcion = ?, FechaModifica = ? WHERE IdCatalogo = ?";
+        String query = "UPDATE Hormiga SET IdSexo = ?, IdProvincia = ?, IdGenoAlimento = ?, IdIngestaNativa = ?, TipoHormiga = ? , Estado = ?,FechaModifica = ? WHERE IdHormiga = ?";
         try {
             Connection          conn = openConnection();
             PreparedStatement pstmt  = conn.prepareStatement(query);
-            pstmt.setString(1, entity.getNombre());
-            pstmt.setString(2, entity.getDescripcion());
-            pstmt.setString(3, dtf.format(now).toString());
-            pstmt.setInt(4, entity.getIdCatalogo());
+            pstmt.setInt(1, entity.getIdSexo());
+            pstmt.setInt(2, entity.getIdProvincia());
+            pstmt.setInt(3, entity.getIdGenoAlimento());
+            pstmt.setInt(4, entity.getIdIngestaNativa());
+            pstmt.setString(5, entity.getTipoHormiga());
+            pstmt.setString(6, entity.getEstado());
+            pstmt.setString(5, dtf.format(now).toString());
+            pstmt.setInt(6, entity.getIdHormiga());
             pstmt.executeUpdate();
             return true;
         } 
         catch (SQLException e) {
-            throw e;// new PatException(e.getMessage(), getClass().getName(), "update()");
+            throw new MMException(e.getMessage(), getClass().getName(), "update()");
         }
     }
 
     @Override
     public boolean delete(int id) throws Exception {
-        String query = " UPDATE Catalogo SET Estado = ? WHERE IdCatalogo = ?";
+        String query = " UPDATE Hormiga SET Estado = ? WHERE IdHormiga = ?";
         try {
             Connection          conn = openConnection();
             PreparedStatement  pstmt = conn.prepareStatement(query);
@@ -99,49 +107,65 @@ public class HormigaDAO extends SQLiteDataHelper implements IDAO<HormigaDTO> {
             return true;
         } 
         catch (SQLException e) {
-            throw e; //new PatException(e.getMessage(), getClass().getName(), "delete()");
+            throw new MMException(e.getMessage(), getClass().getName(), "delete()");
         }
     }
 
     @Override
-    public PersonaTipoDTO readBy(Integer id) throws Exception {
-        PersonaTipoDTO s = new PersonaTipoDTO();
-        String query =" SELECT IdCatalogo   "
-                     +" ,IdCatalogoTipo     "
-                     +" ,Nombre             "
-                     +" ,Descripcion        "
-                     +" ,Estado             "
-                     +" ,FechaCreacion      "
-                     +" ,FechaModifica      "
-                     +" FROM    Catalogo    "
-                     +" WHERE   Estado ='A' "
-                     +" AND     IdCatalogoTipo = 1"
-                     +" AND     IdCatalogo = " + id.toString();  
+    public HormigaDTO readBy(Integer id) throws Exception {
+        HormigaDTO u = new HormigaDTO();
+        String query = 
+         " SELECT RowNum        "
+        +"     ,IdHormiga       "
+        +"     ,IdSexo          "
+        +"     ,IdProvincia     "
+        +"     ,IdGenoAlimento  "
+        +"     ,IdIngestaNativa "
+        +"     ,TipoHormiga     "
+        +"     ,Estado          "
+        +"     ,FechaCrea       "
+        +"     ,FechaModifica   "
+        +" FROM (                   "
+        +"     SELECT ROW_NUMBER() OVER (ORDER BY IdHormiga) AS RowNum "
+        +"     ,IdHormiga       "
+        +"     ,IdSexo          "
+        +"     ,IdProvincia     "
+        +"     ,IdGenoAlimento  "
+        +"     ,IdIngestaNativa "
+        +"     ,TipoHormiga     "
+        +"     ,Estado          "
+        +"     ,FechaCrea       "
+        +"     ,FechaModifica   "  
+        +"     FROM Hormiga       "
+        +"     WHERE Estado = 'A'   "
+        +" ) sub                    "
+        +" WHERE RowNum = " + id.toString();  
         try {
             Connection conn = openConnection();         // conectar a DB     
             Statement  stmt = conn.createStatement();   // CRUD : select * ...    
             ResultSet  rs   = stmt.executeQuery(query);    // ejecutar la
+            //System.out.println(query);
             while (rs.next()) {
-                s = new PersonaTipoDTO( rs.getInt(1)     // IdCatalogo
-                                ,rs.getInt(2)     // IdCatalogoTipo             
-                                ,rs.getString(3)  // Nombre         
-                                ,rs.getString(4)  // Descripcion      
-                                ,rs.getString(5)  // Estado
-                                ,rs.getString(6)  // FechaCreacion
-                                ,rs.getString(7));// FechaModifica
+                u = new HormigaDTO( rs.getInt(1), 
+                                    rs.getInt(2),
+                                    rs.getInt(3), 
+                                    rs.getInt(4),
+                                    rs.getInt(5),
+                                    rs.getString(6), 
+                                    rs.getString(7), 
+                                    rs.getString(8));
             }
         } 
         catch (SQLException e) {
-            throw e; //new PatException(e.getMessage(), getClass().getName(), "readAll()");
+            throw new MMException(e.getMessage(), getClass().getName(), "readBy()");
         }
-        return s; 
+        return u; 
     }
 
     public Integer getRowCount()  throws Exception  {
         String query =" SELECT COUNT(*) TotalReg "
-                     +" FROM    Catalogo         "
-                     +" WHERE   Estado ='A'      "
-                     +" AND     IdCatalogoTipo = 1";
+                     +" FROM    UserLogin         "
+                     +" WHERE   Estado ='A'      ";
         try {
             Connection conn = openConnection();         // conectar a DB     
             Statement  stmt = conn.createStatement();   // CRUD : select * ...    
@@ -151,9 +175,10 @@ public class HormigaDAO extends SQLiteDataHelper implements IDAO<HormigaDTO> {
             }
         } 
         catch (SQLException e) {
-            throw e; //new PatException(e.getMessage(), getClass().getName(), "getMaxRow()");
+            throw new MMException(e.getMessage(), getClass().getName(), "getRowCount()");
         }
         return 0;
     }
 
+    
 }
