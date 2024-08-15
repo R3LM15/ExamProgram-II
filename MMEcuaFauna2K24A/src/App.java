4db35b84164ed13992b4f42ecc7e7b9249f2edb5
R3LM <118556218+R3LM15@users.noreply.github.com>
2024-08-14 23:21:29 -0500

    Corregui el nombre del proy

---
+++ MMEcuaFauna2K24A/src/App.java
@@ -0,0 +1,13 @@
+
+public class App {
+    
+public static String mmcedula = "1723451747" ;
+public static String mmnombres = "Mateo Molina";
+    public static void main(String[] args) throws Exception {
+
+
+
+        System.out.println(mmcedula);
+        System.out.println(mmnombres);
+    }
+}
